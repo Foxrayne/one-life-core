52c2f8a18ebc27ca3de0d952afa73b61f0930325
guspuffygit <guspuffy@gmail.com>
2026-09-13 11:17:19 -0400

    Fix null clothing drop

--- src/main/java/io/pzstorm/storm/core/StormClassTransformers.java
+++ src/main/java/io/pzstorm/storm/core/StormClassTransformers.java
@@ -6,6 +6,7 @@ import io.pzstorm.storm.event.core.PacketEventDispatcher;
 import io.pzstorm.storm.mod.ZomboidMod;
 import io.pzstorm.storm.patch.client.CombatManagerBallisticsNullGuardPatch;
 import io.pzstorm.storm.patch.client.CoreResetLuaPatch;
+import io.pzstorm.storm.patch.client.IsoFallingClothingDropNullGuardPatch;
 import io.pzstorm.storm.patch.client.IsoObjectAdminSeeAllTargetAlphaPatch;
 import io.pzstorm.storm.patch.client.IsoWorldInventoryObjectRenderSpriteGuardPatch;
 import io.pzstorm.storm.patch.client.PlayerDataRequestBackoffPatch;
@@ -565,6 +566,7 @@ public class StormClassTransformers {
             registerTransformer(new IsoObjectAdminSeeAllTargetAlphaPatch());
             registerTransformer(new IsoWorldInventoryObjectRenderSpriteGuardPatch());
             registerTransformer(new CombatManagerBallisticsNullGuardPatch());
+            registerTransformer(new IsoFallingClothingDropNullGuardPatch());
         }
 
         if (StormEnv.isStormServer()) {
